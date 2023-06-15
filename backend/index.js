942b7f210f5e6441a6069d61cf9ddf54554b8af8
jayden-n <jaydennguyen.dev@gmail.com>
2023-06-14 21:51:01 -0400

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const cors = require('cors');
+// Helps backend and client communicate
+// Port: 5000 - backend
+// Client: 3000 - client
+
+const app = express();
+
+// Connect db
+
+mongoose.set('strictQuery', false);
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log('MongoDB has been started!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+connectToMongo();
+
+// Connect server
+app.listen(process.env.PORT || 5001, () => {
+  console.log('Server has been started!');
+});
