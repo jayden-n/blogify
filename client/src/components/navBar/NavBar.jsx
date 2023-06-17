630c4027c8e7d4aef99ffd4aca6cf0883c86425e
jayden-n <jaydennguyen.dev@gmail.com>
2023-06-16 23:25:22 -0400

    designed NavBar with module css

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './navBar.module.css';
+import { Link } from 'react-router-dom';
+import womanImg from '../../assets/woman.jpg';
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.left}>
+          <Link to="/">Blog</Link>
+        </div>
+        <ul className={classes.center}>
+          <li className={classes.listItem}>Home</li>
+          <li className={classes.listItem}>About</li>
+          <li className={classes.listItem}>Contacts</li>
+          <li className={classes.listItem}>Categories</li>
+        </ul>
+        <div className={classes.right}>
+          <img
+            onClick={() => setShowModal((prev) => !prev)}
+            src={womanImg}
+            alt="woman"
+            className={classes.img}
+          />
+          <Link to="/create">Create</Link>
+          <span>Logout</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import classes from './home.module.css';
+import NavBar from '../../components/navBar/NavBar';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default Home;
