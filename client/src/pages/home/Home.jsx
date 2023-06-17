033749e0cf955cf9892a3329581244df0c0d7a65
jayden-n <jaydennguyen.dev@gmail.com>
2023-06-16 23:29:18 -0400

    planned out FeaturedBlog structure

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './home.module.css';
 import NavBar from '../../components/navBar/NavBar';
+import FeaturedBlogs from '../../components/featuredBlogs/FeaturedBlogs';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <FeaturedBlogs />
     </div>
   );
 };
