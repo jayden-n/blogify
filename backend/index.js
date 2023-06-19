17e3691314e8417b71f76e4dec6b29a9ca88db31
jayden-n <jaydennguyen.dev@gmail.com>
2023-06-18 23:39:43 -0400

    added logic for choosing types of blog category

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 const authController = require('./controllers/authController');
 const blogController = require('./controllers/blogController');
+const multer = require('multer');
 // Helps backend and client communicate
 // Port: 5000 - backend
 // Client: 3000 - client
@@ -25,12 +26,32 @@ const connectToMongo = async () => {
 };
 connectToMongo();
 // Routes
+app.use('/images', express.static('public/images'));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authController);
 app.use('/blog', blogController);
+
+// Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.body.filename);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+  return res.status(200).json({ msg: 'Successfully uploaded' });
+});
 // Connect server
 app.listen(process.env.PORT || 5001, () => {
   console.log('Server has been started!');
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import classes from './home.module.css';
 import NavBar from '../../components/navBar/NavBar';
 import FeaturedBlogs from '../../components/featuredBlogs/FeaturedBlogs';
+import Categories from '../../components/categories/Categories';
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <FeaturedBlogs />
+      <Categories />
     </div>
   );
 };
